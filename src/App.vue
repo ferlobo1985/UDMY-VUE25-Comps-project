d69c23cf479a7c4208ee3716952e46d0c9bacfac
Coding rev <fernando.lobo.prez@gmail.com>
2025-08-08 00:24:46 -0300

    04-The style Tag
    04-The style Tag

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
     <div>
         <app-header/>
-        <div>
+        <div class="container">
             Content
         </div>
-        <Footer/>
     </div>
 </template>
 
 <script setup>
-    import Footer from './components/header_footer/Footer.vue';
+
 </script>
 
 <style>
+    body {
+        padding:0;
+        margin:0;
+        font-family: 'Roboto',sans-serif;
+    }
+    .container {
+        min-height:84vh;
+        box-sizing: border-box;
+        padding: 20px;
+    }
 </style>
--- src/components/header_footer/Header.vue
+++ src/components/header_footer/Header.vue
@@ -5,3 +5,18 @@
         </div>
     </header>
 </template>
+
+
+<style scoped>
+    header {
+        background: #2196f6;
+        border-bottom: 4px solid #607D8B;
+        box-sizing: border-box;
+        padding: 30px;
+        color: white;
+        font-size: 30px;
+    }
+</style>
+
+
+
