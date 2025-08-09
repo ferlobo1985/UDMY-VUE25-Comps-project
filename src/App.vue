02ab53568a42cf41b3019beb4b507d05d4017c8a
Coding rev <fernando.lobo.prez@gmail.com>
2025-08-09 13:33:14 -0300

    18-Custom directives
    18-Custom directives

--- src/App.vue
+++ src/App.vue
@@ -2,26 +2,13 @@
     <div>
         <app-header/>
         <div class="container">
-
-            <button @click="activeComp=Mike">Mike</button>
-            <button @click="activeComp=Steve">Steve</button>
-            <!-- <Mike v-if="activeComp === Mike"/>
-            <Steve v-if="activeComp === Steve"/> -->
-            <KeepAlive include="mike,steve">
-                <component :is="activeComp"/>
-            </KeepAlive>
-           
-
+            <Directives/>
         </div>
     </div>
 </template>
 
 <script setup>
-    import Mike from '@/components/Players/mike.vue';
-    import Steve from '@/components/Players/steve.vue';
-    import { ref, shallowRef } from 'vue';
-
-    const activeComp = shallowRef(Mike)
+    import Directives from '@/components/Directives/index.vue'
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -6,5 +6,14 @@ import Header from "./components/header_footer/Header.vue";
 const app = createApp(App);
 
 
+app.directive('awesome',{
+    beforeMount(el,binding){
+        el.style.color = binding.modifiers.red ? 'red':'blue';
+        el.style.fontSize = binding.modifiers.big ? '20px':'10px'
+
+        el.innerHTML = binding.value
+    }
+})
+
 app.component('app-header',Header)
 app.mount('#app')
